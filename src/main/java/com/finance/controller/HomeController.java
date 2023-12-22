e333e5f752ea2aa7351d65a7fc49686250f79452
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2023-12-22 13:33:06 +0530

    Initial push

---
+++ src/main/java/com/finance/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.finance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("title", "Aarav Finance- Home");
+		return "index";
+	}
+}
