f6c0d6921c672332c607f4f6a111395eef2c9f41
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2024-01-08 19:23:42 +0530

    updated

--- src/main/java/com/finance/controller/HomeController.java
+++ src/main/java/com/finance/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.finance.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class HomeController {
 	public String index(Model model) {
 		model.addAttribute("title", "Aarav Finance- Home");
 		return "index";
+	}
+	
+	@GetMapping("/admin_panel")
+	public String admin() {
+		return "adminD";
 	}
 }
